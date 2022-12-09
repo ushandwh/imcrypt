6df014f2d70342b0c74019324624af8baf00766c
Ushan Rajapakshe <78900168+ushandwh@users.noreply.github.com>
2022-12-09 05:24:11 -0500

    Update init.js

--- utils/init.js
+++ utils/init.js
@@ -5,7 +5,7 @@ const unhandled = require('cli-handle-unhandled');
 module.exports = ({ clear = true }) => {
 	unhandled();
 	welcome({
-		title: `imcrypt`,
+		title: `wecrypt`,
 		tagLine: `by UnS`,
 		description: pkg.description,
 		version: pkg.version,
