80e3e696c5a49c7bde516e5c6b2ba651eef1cb6c
Ushan Rajapakshe <78900168+ushandwh@users.noreply.github.com>
2022-12-09 05:20:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,4 +32,11 @@ const { clear } = flags;
 
 	const chalk = (await import(`chalk`)).default;
 
+	// print Give it a star on github: https://github.com/theninza/imcrypt with chalk and bgMagenta
+	console.log(
+		chalk.bgMagenta(` ISP_IE3092_Module_Project_Teams :  `) +
+			chalk.bold(` 
+https://teams.microsoft.com/l/channel/19%3a71b2b3b0872441a9b05df8c9f07c2acf%40thread.tacv2/2022_S2_G83?groupId=f4770004-86a3-44ff-936b-5881a910cbd4&tenantId=44e3cf94-19c9-4e32-96c3-14f5bf01391a `)
+	);
+
 })();
